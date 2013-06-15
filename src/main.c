b3d07872ced3a1dc7db69f78b7a7f96042e01260
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2013-06-15 14:54:29 -0700

    Option parsing for authinfo tool.

--- src/main.c
+++ src/main.c
@@ -1,92 +1,100 @@
 /* -*- mode: c; c-basic-offset: 4; tab-width: 4; indent-tabs-mode: nil; -*- */
 
+#include <assert.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
+#include <getopt.h>
+
 #include "authinfo.h"
 
-static bool
-entry_callback(const struct authinfo_parse_entry_t *entry, void *arg)
-{
-    const char *password;
-    enum authinfo_result_t ret;
+enum command_t {
+    CMD_QUERY,
+    CMD_VALIDATE,
+    CMD_VERSION,
+    CMD_HELP,
+};
 
-    if (entry->password != NULL) {
-        ret = authinfo_password_extract(entry->password, &password);
-        if (ret != AUTHINFO_OK) {
-            printf("Could not extract password from entry: %s\n",
-                   authinfo_strerror(ret));
-            password = NULL;
-        }
-    } else {
-        password = NULL;
-    }
+enum option_t {
+    OPT_USER = 1,
+    OPT_HOST,
+    OPT_PROTOCOL,
+    OPT_PATH,
+};
 
-    printf("Entry: host %s, protocol %s, user %s, password %s, force %s\n",
-           entry->host, entry->protocol, entry->user, password,
-           (entry->force ? "true" : "false"));
-    /* don't stop */
-    return false;
-}
+static const char *program = NULL;
 
-static bool
-error_callback(const struct authinfo_parse_error_t *error, void *arg)
-{
-    printf("Error in line %u, column %u: %s\n",
-           error->line, error->column, authinfo_parse_strerror(error->type));
-    /* don't stop */
-    return false;
-}
+static int command = CMD_HELP;
+
+static const char *authinfo_path = NULL;
+
+static const char *user = NULL;
+static const char *host = NULL;
+static const char *protocol = NULL;
 
 static void
-parse_file(const char *path)
+usage(void)
 {
-    char buf[10240];
-    enum authinfo_result_t ret;
-
-    ret = authinfo_read_file(path, buf, sizeof(buf));
-    if (ret != AUTHINFO_OK) {
-        printf("Could not read authinfo file: %s\n", authinfo_strerror(ret));
-        return;
-    }
-
-    printf("\n============================================================\n");
-    printf("%s\n", path);
-    printf("============================================================\n");
-    printf("%s", buf);
-
-    printf("\n=========================parse results=========================\n");
-    authinfo_parse(buf, NULL, entry_callback, error_callback);
+    printf("Usage: %s [COMMAND] [OPTIONS]\n\n", program);
+    printf("Supported commands:\n");
+    printf("   --query      query authinfo file for matching entries\n");
+    printf("        --user             match user name\n");
+    printf("        --host             match host name\n");
+    printf("        --protocol         match protocol\n");
+    printf("        --path             use this authinfo file instead of autodiscovered\n");
+    printf("   --validate   check authinfo file for syntax errors\n");
+    printf("        --path             use this authinfo file instead of autodiscovered\n");
+    printf("   --version    print version info\n");
+    printf("   --help       print this help\n");
 }
 
 int
-main(void)
+main(int argc, char *argv[])
 {
-    char *authinfo_path;
-    int ret;
+    struct option options[] = {
+        {"query", no_argument, &command, CMD_QUERY},
+        {"validate", no_argument, &command, CMD_VALIDATE},
+        {"version", no_argument, &command, CMD_VERSION},
+        {"help", no_argument, &command, CMD_HELP},
+        {"user", required_argument, NULL, OPT_USER},
+        {"host", required_argument, NULL, OPT_HOST},
+        {"protocol", required_argument, NULL, OPT_PROTOCOL},
+        {"path", required_argument, NULL, OPT_PATH},
+        {0, 0, 0, 0}
+    };
 
-    ret = authinfo_init();
-    if (ret != AUTHINFO_OK) {
-        printf("Couldn't initialize authinfo library: %s\n",
-               authinfo_strerror(ret));
-        return EXIT_FAILURE;
+    program = argv[0];
+
+    while (true) {
+        int opt;
+
+        opt = getopt_long(argc, argv, "", options, NULL);
+        if (opt == '?') {
+            return EXIT_FAILURE;
+        } else if (opt == -1) {
+            break;
+        }
+
+        switch (opt) {
+        case 0:
+            break;
+        case OPT_USER:
+            user = optarg;
+            break;
+        case OPT_HOST:
+            host = optarg;
+            break;
+        case OPT_PROTOCOL:
+            protocol = optarg;
+            break;
+        case OPT_PATH:
+            authinfo_path = optarg;
+            break;
+        default:
+            /* should not happen */
+            assert(false);
+        }
     }
 
-    ret = authinfo_find_file(&authinfo_path);
-    switch (ret) {
-    case AUTHINFO_OK:
-        printf("Found authinfo file at %s\n", authinfo_path);
-        parse_file(authinfo_path);
-        ret = EXIT_SUCCESS;
-        break;
-    case AUTHINFO_ENOENT:
-        printf("Couldn't find authinfo file\n");
-        ret = EXIT_SUCCESS;
-        break;
-    default:
-        printf("Got unexpected error while "
-               "looking for authinfo file: %s\n", authinfo_strerror(ret));
-        ret = EXIT_FAILURE;
-    }
-
-    return ret;
+    return EXIT_SUCCESS;
 }
